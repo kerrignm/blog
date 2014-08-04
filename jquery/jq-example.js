d963e9184d98dce961541d684f4969cdb248e1ac
kerrignm <kerrignm@hotmail.com>
2014-08-04 11:07:07 +0800

    update

--- jquery/jq-example.js
+++ jquery/jq-example.js
@@ -2,6 +2,5 @@ $(document).ready(function(){
     alert("document ready!");
     $('#btn_01').click(function(){
         alert("button click!");
-        $('#div_01').load('/blog/jquery/text.txt');
     });
 });
