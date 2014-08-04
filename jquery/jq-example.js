e236ab6f7f08355f12793bb733cc75a296700198
kerrignm <kerrignm@hotmail.com>
2014-08-04 10:51:21 +0800

    update

--- jquery/jq-example.js
+++ jquery/jq-example.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $("#btn_01").click(function(){
-  $('#div_01').load('/blog/jquery/text.txt');
-  });
-})
+    $("#btn_01").click(function(){
+        $('#div_01').load('/blog/jquery/text.txt');
+    });
+});
